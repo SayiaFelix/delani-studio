a0c7d3036638445c846dac7b50a8495ccb608f9e
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-03-12 19:17:09 +0300

    Scripting the what about div

--- JS/script.js
+++ JS/script.js
@@ -49,36 +49,91 @@ $(document).ready(() => {
     });
 
 
+    // project1
+    $("#work1-image").mouseenter(
+        function() {
+
+            $("#portfolio").slideDown('slow').css("display", "block");
+        })
+    $("#work1-image").mouseleave(function(e) {
+        e.preventDefault();
+        $("#portfolio").hide().break();
+    });
+
+    // project 2
+    $("#work2-image").mouseenter(
+        function() {
+            $("#portfolio").slideDown('slow').css("display", "block");
+        })
+    $("#work2-image").mouseleave(function() {
+        $("#portfolio").hide();
+    });
+
+    // project 3
+    $("#work3-image").mouseenter(
+        function() {
+            $("#portfolio").slideDown('slow').css("display", "block");
+        })
+    $("#work3-image").mouseleave(function() {
+        $("#portfolio").hide();
+    });
 
 
-    // $("work1-image").hover(function() {
+    // project 4
+    $("#work4-image").mouseenter(
+        function() {
+            $("#portfolio").slideDown('slow').css("display", "block");
+        })
+    $("#work4-image").mouseleave(function() {
+        $("#portfolio").hide();
+    });
 
-    //     $("#content1").show();
-
-
-    // });
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // project 5
+    $("#work5-image").mouseenter(
+        function() {
+            $("#portfolio").slideDown('slow').css("display", "block");
+        })
+    $("#work5-image").mouseleave(function() {
+        $("#portfolio").hide();
+    });
 
 
+    // project 6
+    $("#work6-image").mouseenter(
+        function() {
+            $("#portfolio").slideDown('slow').css("display", "block");
+        })
+    $("#work6-image").mouseleave(function() {
+        $("#portfolio").hide();
+    });
+
+    // project 7
+    $("#work7-image").mouseenter(
+        function() {
+            $("#portfolio").slideDown('slow').css("display", "block");
+        })
+    $("#work7-image").mouseleave(function() {
+        $("#portfolio").hide();
+    });
+
+    // project 8
+
+    $("#work8-image").mouseenter(
+        function() {
+            $("#portfolio").slideDown('slow').css("display", "block");
+        })
+    $("#work8-image").mouseleave(function() {
+        $("#portfolio").hide();
+    });
 
 
-
-
-
-
-
-
+    // $("#work-image").hover(
+    //     function() {
+    //         $("#portfolio").css("display", "none").slideDown(1000);
+    //     },
+    //     function() {
+    //         $("#portfolio").css("display", "block").slideUp(slow);
+    //     }
+    // );
 
 });
