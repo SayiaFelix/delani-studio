b0640b507455fc2cf12cf4b69aafa9ab379da135
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-03-13 08:10:24 +0300

    Scripting the what about div

--- app.js
+++ app.js
@@ -138,12 +138,16 @@ $(document).ready(() => {
 
 
 
+    // API SECTION
+    const express = require('express');
+    const request = require('request');
+    const bodyParser = require('body-parser');
+    const path = require('path');
 
+    const app = express();
 
-
-
-
-
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, console.log(`server started on ${PORT}`))
 
 
 
